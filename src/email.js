1fb0409fda922e0a53640a4d29807bf9993c7232
Majt <ismjt@qq.com>
2017-04-08 23:15:17 +0800

    私有配置文件gitignore

--- src/email.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Majt on 2017/4/8.
- */
-export default {
-  service: 'smtp.qq.com', // SMTP服务提供商域名
-  username: '2460756073@qq.com', // 用户名/用户邮箱
-  password: 'qfluboqqrdipdihc', // 邮箱授权码
-  sender_address: '"智慧590项目 APP开发小组 👥" <m@ismjt.cn>'
-}
